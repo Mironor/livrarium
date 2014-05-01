0ab3dd7b86fc943e6b2c4c3a4318499b273f2c76
Alexandre Bedrytski <alexandre@zenprog.fr>
2014-05-01 18:41:47 +0200

    Implemented AuthToken model that uses SecureSocial Token as a base case class

--- app/models/Token.scala
+++ app/models/Token.scala
@@ -10,52 +10,36 @@ import com.novus.salat.dao._
 import com.mongodb.casbah.commons.Imports._
 import com.mongodb.casbah.MongoConnection
 
-import securesocial.core._
-import securesocial.core.IdentityId
+import securesocial.core.providers.Token
 import org.bson.types.ObjectId
+import com.mongodb.casbah.commons.conversions.scala.{RegisterJodaTimeConversionHelpers, RegisterConversionHelpers}
 
-case class Token(_id: ObjectId = new ObjectId,
-                identityId: IdentityId,
-                firstName: String,
-                lastName: String,
-                fullName: String,
-                email: Option[String],
-                avatarUrl: Option[String],
-                authMethod: AuthenticationMethod,
-                oAuth1Info: Option[OAuth1Info],
-                oAuth2Info: Option[OAuth2Info],
-                passwordInfo: Option[PasswordInfo]) extends Identity {}
-
-object TokenDAO extends SalatDAO[Token, ObjectId](
+object AuthTokenDAO extends SalatDAO[Token, ObjectId](
   collection = MongoConnection()(
     current.configuration.getString("mongodb.default.db")
       .getOrElse(throw new PlayException("Configuration error",
       "Could not find mongodb.default.db in settings"))
-  )("tasks"))
+  )("auth_tokens"))
 
-object Token {
-  def all(): List[Token] = TokenDAO.find(MongoDBObject.empty).toList
+object AuthToken {
+  def all(): List[Token] = AuthTokenDAO.find(MongoDBObject.empty).toList
 
-  def find(id: IdentityId): Option[Identity] = {
-    TokenDAO.findOne(MongoDBObject(
-      "identityId.userId" -> id.userId,
-      "identityId.providerId" -> id.providerId
+  def find(uuid: String): Option[Token] = {
+    AuthTokenDAO.findOne(MongoDBObject(
+      "uuid" -> uuid
     ))
   }
 
-  def findByEmailAndProvider(email: String, providerId: String): Option[Identity] = {
-    TokenDAO.findOne(MongoDBObject("email" -> email, "identityId.providerId" -> providerId))
+  def save(token: Token) = {
+    RegisterConversionHelpers()
+    RegisterJodaTimeConversionHelpers()
+    AuthTokenDAO.insert(token)
+    token
   }
 
-  def save(user: Identity) = {
-    TokenDAO.insert(user.asInstanceOf[Token])
-    user
-  }
-
-  def delete(id: IdentityId) = {
-    TokenDAO.remove(MongoDBObject(
-      "identityId.userId" -> id.userId,
-      "identityId.providerId" -> id.providerId
+  def delete(uuid: String) = {
+    AuthTokenDAO.remove(MongoDBObject(
+      "uuid" -> uuid
     ))
   }
 }
