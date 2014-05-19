26f2a55ed24f1d9cc9cd8bcfa97dbf96e723a4cb
Alexandre Bedrytski <alexandre@zenprog.fr>
2014-05-19 08:45:02 +0200

    added first test and working karma configuration

---
+++ public/js/app/modules/upload-button/upload-button.spec.js
@@ -0,0 +1,18 @@
+describe('Upload Button', function () {
+    var $compile, $rootScope;
+
+    beforeEach(module('lvr.uploadButton'));
+
+    beforeEach(inject(function (_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('contains "upload" word', function () {
+        var element = $compile('<upload-button></upload-button>')($rootScope);
+
+        $rootScope.$digest();
+
+        expect(element.html()).toContain("upload");
+    })
+});
